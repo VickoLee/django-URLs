b03c3667cea635d3e96788b48e1e01e439cbfd9d
Uche Victory <uchevictory4@gmail.com>
2022-06-18 10:17:21 +0100

    new commit

---
+++ Vee/Vee/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Vee project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vee.settings')
+
+application = get_asgi_application()
